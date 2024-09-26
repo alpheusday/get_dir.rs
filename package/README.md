c76ae8b02af1dc9ad3ee4155fd4a004d7155a311
Alpheus <26622568+alpheustangs@users.noreply.github.com>
2024-09-27 02:33:19 +0800

    Documentation updates

--- package/README.md
+++ package/README.md
@@ -1,6 +1,12 @@
 # Get Dir
 
-A utility to get directory.
+An utility to get directory.
+
+A Directory searching utility that will check whether 
+the target file or directory exists in the directory. 
+The search process will start from the current directory and go to the root. 
+Therefore, targets in other subdirectories will not be found, 
+but a better performance is expected.
 
 ## Usage
 
--- package/src/lib.rs
+++ package/src/lib.rs
@@ -1,21 +1,27 @@
 //! # Get Dir
-//! 
-//! A utility to get directory.
-//! 
+//!
+//! An utility to get directory.
+//!
+//! A Directory searching utility that will check whether
+//! the target file or directory exists in the directory.
+//! The search process will start from the current directory and go to the root.
+//! Therefore, targets in other subdirectories will not be found,
+//! but a better performance is expected.
+//!
 //! ## Usage
-//! 
+//!
 //! Get directory by target with the following code:
-//! 
+//!
 //! ```no_run
 //! use get_dir::{
 //!     Target,
 //!     TargetType,
 //!     get_dir_by_target,
 //! };
-//! 
+//!
 //! // Get the directory of the `LICENSE` file located in.
-//! get_dir_by_target(Target { 
-//!     name: "LICENSE".to_string(), 
+//! get_dir_by_target(Target {
+//!     name: "LICENSE".to_string(),
 //!     ty: TargetType::File,
 //! });
 //! ```
